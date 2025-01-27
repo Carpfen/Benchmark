a69639f0e9e0c20735de0b55870d7bb163b1b6bd
Carpfen <salbeitee135@gmx.de>
2025-01-27 12:14:31 +0100

    Initial Commit

---
+++ Benchmark.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Benchmark
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        CPUBtn = New Button()
+        RAMBtn = New Button()
+        DiskBtn = New Button()
+        TestAllBtn = New Button()
+        CPULbl = New Label()
+        RAMLbl = New Label()
+        DiskLbl = New Label()
+        InfoBtn = New Button()
+        CopyBtn = New Button()
+        SuspendLayout()
+        ' 
+        ' CPUBtn
+        ' 
+        CPUBtn.BackColor = SystemColors.Control
+        CPUBtn.Cursor = Cursors.Hand
+        CPUBtn.Font = New Font("Microsoft Sans Serif", 24.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        CPUBtn.Location = New Point(58, 29)
+        CPUBtn.Margin = New Padding(4, 3, 4, 3)
+        CPUBtn.Name = "CPUBtn"
+        CPUBtn.RightToLeft = RightToLeft.No
+        CPUBtn.Size = New Size(292, 87)
+        CPUBtn.TabIndex = 0
+        CPUBtn.Text = "Teste CPU:"
+        CPUBtn.UseVisualStyleBackColor = False
+        ' 
+        ' RAMBtn
+        ' 
+        RAMBtn.Cursor = Cursors.Hand
+        RAMBtn.Font = New Font("Microsoft Sans Serif", 24.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        RAMBtn.Location = New Point(58, 144)
+        RAMBtn.Margin = New Padding(4, 3, 4, 3)
+        RAMBtn.Name = "RAMBtn"
+        RAMBtn.Size = New Size(292, 87)
+        RAMBtn.TabIndex = 1
+        RAMBtn.Text = "Teste RAM:"
+        RAMBtn.UseVisualStyleBackColor = False
+        ' 
+        ' DiskBtn
+        ' 
+        DiskBtn.Cursor = Cursors.Hand
+        DiskBtn.Font = New Font("Microsoft Sans Serif", 24.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        DiskBtn.Location = New Point(58, 260)
+        DiskBtn.Margin = New Padding(4, 3, 4, 3)
+        DiskBtn.Name = "DiskBtn"
+        DiskBtn.Size = New Size(292, 87)
+        DiskBtn.TabIndex = 2
+        DiskBtn.Text = "Teste Disk:"
+        DiskBtn.UseVisualStyleBackColor = False
+        ' 
+        ' TestAllBtn
+        ' 
+        TestAllBtn.Cursor = Cursors.Hand
+        TestAllBtn.Font = New Font("Microsoft Sans Serif", 24.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TestAllBtn.Location = New Point(58, 375)
+        TestAllBtn.Margin = New Padding(4, 3, 4, 3)
+        TestAllBtn.Name = "TestAllBtn"
+        TestAllBtn.Size = New Size(292, 87)
+        TestAllBtn.TabIndex = 3
+        TestAllBtn.Text = "Teste Alles"
+        TestAllBtn.UseVisualStyleBackColor = False
+        ' 
+        ' CPULbl
+        ' 
+        CPULbl.Font = New Font("Microsoft Sans Serif", 24.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        CPULbl.Location = New Point(496, 29)
+        CPULbl.Margin = New Padding(4, 0, 4, 0)
+        CPULbl.Name = "CPULbl"
+        CPULbl.Size = New Size(292, 87)
+        CPULbl.TabIndex = 4
+        CPULbl.Text = "CPU-Werte"
+        CPULbl.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' RAMLbl
+        ' 
+        RAMLbl.Font = New Font("Microsoft Sans Serif", 24.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        RAMLbl.Location = New Point(496, 144)
+        RAMLbl.Margin = New Padding(4, 0, 4, 0)
+        RAMLbl.Name = "RAMLbl"
+        RAMLbl.Size = New Size(292, 87)
+        RAMLbl.TabIndex = 5
+        RAMLbl.Text = "RAM-Werte"
+        RAMLbl.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' DiskLbl
+        ' 
+        DiskLbl.Font = New Font("Microsoft Sans Serif", 24.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        DiskLbl.Location = New Point(496, 260)
+        DiskLbl.Margin = New Padding(4, 0, 4, 0)
+        DiskLbl.Name = "DiskLbl"
+        DiskLbl.Size = New Size(292, 87)
+        DiskLbl.TabIndex = 6
+        DiskLbl.Text = "Disk-Werte"
+        DiskLbl.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' InfoBtn
+        ' 
+        InfoBtn.Cursor = Cursors.Hand
+        InfoBtn.Font = New Font("Microsoft Sans Serif", 24.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        InfoBtn.Location = New Point(58, 490)
+        InfoBtn.Margin = New Padding(4, 3, 4, 3)
+        InfoBtn.Name = "InfoBtn"
+        InfoBtn.Size = New Size(292, 87)
+        InfoBtn.TabIndex = 7
+        InfoBtn.Text = "Informationen"
+        InfoBtn.UseVisualStyleBackColor = False
+        ' 
+        ' CopyBtn
+        ' 
+        CopyBtn.Cursor = Cursors.Hand
+        CopyBtn.Font = New Font("Microsoft Sans Serif", 24.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        CopyBtn.Location = New Point(496, 375)
+        CopyBtn.Margin = New Padding(4, 3, 4, 3)
+        CopyBtn.Name = "CopyBtn"
+        CopyBtn.Size = New Size(292, 87)
+        CopyBtn.TabIndex = 8
+        CopyBtn.Text = "Kopiere Werte"
+        CopyBtn.UseVisualStyleBackColor = False
+        ' 
+        ' Benchmark
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(856, 599)
+        Controls.Add(CopyBtn)
+        Controls.Add(InfoBtn)
+        Controls.Add(DiskLbl)
+        Controls.Add(RAMLbl)
+        Controls.Add(CPULbl)
+        Controls.Add(TestAllBtn)
+        Controls.Add(DiskBtn)
+        Controls.Add(RAMBtn)
+        Controls.Add(CPUBtn)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Margin = New Padding(4, 3, 4, 3)
+        MaximizeBox = False
+        Name = "Benchmark"
+        Text = "Benchmark"
+        ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CPUBtn As Button
+    Friend WithEvents RAMBtn As Button
+    Friend WithEvents DiskBtn As Button
+    Friend WithEvents TestAllBtn As Button
+    Friend WithEvents CPULbl As Label
+    Friend WithEvents RAMLbl As Label
+    Friend WithEvents DiskLbl As Label
+    Friend WithEvents InfoBtn As Button
+    Friend WithEvents CopyBtn As Button
+End Class
---
+++ Benchmark.vb
@@ -0,0 +1,205 @@
+﻿Imports System.IO
+
+Public Class Benchmark
+    Dim StartTime As DateTime
+    Dim Version = "1.3"
+
+    Public Sub CPU()
+        Dim n As Long
+        Dim time As Double
+        CPULbl.Text = "Berechne . . ."
+        CPULbl.BackColor = CPUBtn.BackColor
+        CPULbl.Update()
+        StartTime = Now
+        For i As Long = 0 To 2000000000
+            n = i + i * 2
+        Next
+        time = Now.Subtract(StartTime).TotalSeconds
+        CPULbl.Text = time.ToString.Substring(0, 4)
+        If time < 3 Then
+            CPULbl.BackColor = Color.Green
+        ElseIf time < 6 Then
+            CPULbl.BackColor = Color.Yellow
+        Else
+            CPULbl.BackColor = Color.Red
+        End If
+        CPULbl.Update()
+    End Sub
+
+    Public Sub RAM()
+        Dim list As New ArrayList()
+        Dim time As Double
+        RAMLbl.Text = "Berechne . . ."
+        RAMLbl.BackColor = RAMBtn.BackColor
+        RAMLbl.Update()
+        StartTime = Now
+        For i As Long = 0 To 15000000
+            list.Add(1)
+        Next
+        time = Now.Subtract(StartTime).TotalSeconds
+        RAMLbl.Text = time.ToString.Substring(0, 4)
+        If time < 3 Then
+            RAMLbl.BackColor = Color.Green
+        ElseIf time < 6 Then
+            RAMLbl.BackColor = Color.Yellow
+        Else
+            RAMLbl.BackColor = Color.Red
+        End If
+        RAMLbl.Update()
+    End Sub
+
+    Public Sub Disk()
+        Dim time As Double
+        DiskLbl.Text = "Berechne . . ."
+        DiskLbl.BackColor = CPUBtn.BackColor
+        DiskLbl.Update()
+        StartTime = Now
+
+        Dim writer As StreamWriter
+        writer = New IO.StreamWriter("tempfile.ckbm", False)
+        For i As Long = 0 To 18000000
+            writer.Write("1")
+        Next
+        writer.Close()
+        writer.Dispose()
+
+        Dim reader As StreamReader
+        reader = New IO.StreamReader("tempfile.ckbm")
+        For i As Long = 0 To 500000
+            reader.ReadToEnd()
+        Next
+        reader.Close()
+        reader.Dispose()
+
+        File.Delete("tempfile.ckbm")
+        time = Now.Subtract(StartTime).TotalSeconds
+        DiskLbl.Text = time.ToString.Substring(0, 4)
+        If time < 3 Then
+            DiskLbl.BackColor = Color.Green
+        ElseIf time < 6 Then
+            DiskLbl.BackColor = Color.Yellow
+        Else
+            DiskLbl.BackColor = Color.Red
+        End If
+        DiskLbl.Update()
+    End Sub
+
+    Public Sub Enable(enabled As Boolean)
+        CPUBtn.Enabled = enabled
+        RAMBtn.Enabled = enabled
+        DiskBtn.Enabled = enabled
+        TestAllBtn.Enabled = enabled
+        InfoBtn.Enabled = enabled
+        CopyBtn.Enabled = enabled
+    End Sub
+
+    Private Sub CPUBtn_Click(sender As Object, e As EventArgs) Handles CPUBtn.Click
+        Enable(False)
+        CPU()
+        Enable(True)
+        CPUBtn.Focus()
+    End Sub
+
+    Private Sub RAMBtn_Click(sender As Object, e As EventArgs) Handles RAMBtn.Click
+        Enable(False)
+        RAM()
+        Enable(True)
+        RAMBtn.Focus()
+    End Sub
+
+    Private Sub DiskBtn_Click(sender As Object, e As EventArgs) Handles DiskBtn.Click
+        Enable(False)
+        Disk()
+        Enable(True)
+        DiskBtn.Focus()
+    End Sub
+
+    Private Sub TestAllBtn_Click(sender As Object, e As EventArgs) Handles TestAllBtn.Click
+        Enable(False)
+        CPU()
+        RAM()
+        Disk()
+        Enable(True)
+        TestAllBtn.Focus()
+    End Sub
+
+    Private Sub Benchmark_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text &= $" ({Version})"
+        Me.Show()
+        TestAllBtn.Focus()
+    End Sub
+
+    Private Sub InfoBtn_Click(sender As Object, e As EventArgs) Handles InfoBtn.Click
+        MsgBox($"Ersteller des Benchmarks:{vbCrLf}Carsten Kranz{vbCrLf & vbCrLf}Version:{vbCrLf & Version & vbCrLf & vbCrLf & vbCrLf}Die Werte geben die Zeit des jeweiligen Prozesses in Sekunden an.{vbCrLf}Doppelklicken Sie die Werte, um sie in der Zwischenablage zu speichern.", vbInformation, "Informationen")
+    End Sub
+
+    Private Sub CPULbl_DoubleClick(sender As Object, e As EventArgs) Handles CPULbl.DoubleClick
+        Dim t As String
+        If IsNumeric(CPULbl.Text.ToCharArray()(0)) Then
+            t = CPULbl.Text
+            Clipboard.SetText(t)
+            CPULbl.Text = "Kopiert!"
+            CPULbl.Update()
+            Threading.Thread.Sleep(1000)
+            CPULbl.Text = t
+        Else
+            Clipboard.Clear()
+        End If
+    End Sub
+
+    Private Sub RAMLbl_DoubleClick(sender As Object, e As EventArgs) Handles RAMLbl.DoubleClick
+        Dim t As String
+        If IsNumeric(RAMLbl.Text.ToCharArray()(0)) Then
+            t = RAMLbl.Text
+            Clipboard.SetText(t)
+            RAMLbl.Text = "Kopiert!"
+            RAMLbl.Update()
+            Threading.Thread.Sleep(1000)
+            RAMLbl.Text = t
+        Else
+            Clipboard.Clear()
+        End If
+    End Sub
+
+    Private Sub DiskLbl_DoubleClick(sender As Object, e As EventArgs) Handles DiskLbl.DoubleClick
+        Dim t As String
+        If IsNumeric(DiskLbl.Text.ToCharArray()(0)) Then
+            t = DiskLbl.Text
+            Clipboard.SetText(t)
+            DiskLbl.Text = "Kopiert!"
+            DiskLbl.Update()
+            Threading.Thread.Sleep(1000)
+            DiskLbl.Text = t
+        Else
+            Clipboard.Clear()
+        End If
+    End Sub
+
+    Private Sub CopyBtn_Click(sender As Object, e As EventArgs) Handles CopyBtn.Click
+        Dim cpu As String
+        Dim ram As String
+        Dim disk As String
+
+        If IsNumeric(CPULbl.Text.ToCharArray()(0)) Then
+            cpu = CPULbl.Text
+        Else
+            cpu = "-"
+        End If
+        If IsNumeric(RAMLbl.Text.ToCharArray()(0)) Then
+            ram = RAMLbl.Text
+        Else
+            ram = "-"
+        End If
+        If IsNumeric(DiskLbl.Text.ToCharArray()(0)) Then
+            disk = DiskLbl.Text
+        Else
+            disk = "-"
+        End If
+
+        Clipboard.SetText($"Benchmark ({Version}) Ergebnisse:{vbCrLf}CPU: {cpu & vbCrLf}RAM: {ram & vbCrLf}Disk: {disk}")
+        CopyBtn.Text = "Kopiert!"
+        CopyBtn.Update()
+        Threading.Thread.Sleep(1000)
+        CopyBtn.Text = "Kopiere Werte"
+    End Sub
+End Class
